6a341e4a19558d1b0ae2de8ad2d29a9b08d86d6f
Abdel-RahmanSaied <abdo.am169@gmail.com>
2023-12-30 15:19:01 +0300

    handle icon logo

--- __main__.py
+++ __main__.py
@@ -14,7 +14,7 @@ class User_Analyser(QtWidgets.QStackedWidget):
         self.resize(800, 650)
         # self.setMaximumSize(QtCore.QSize(800, 650))
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/icons/birdt.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/icons/icons/birdt.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setWindowIcon(icon)
         _translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(_translate("Form", "User Analyzer "))
@@ -22,7 +22,6 @@ class User_Analyser(QtWidgets.QStackedWidget):
         self.main_manager = MainManager()
 
         self.check_internet_connection()
-
 
         # self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 
@@ -50,7 +49,6 @@ class User_Analyser(QtWidgets.QStackedWidget):
     #     except Exception as e:
     #         print(e)
 
-
     def check_internet_connection(self):
         manager = QNetworkAccessManager(self)
         request = QNetworkRequest(QUrl("http://www.google.com"))
@@ -77,11 +75,11 @@ class User_Analyser(QtWidgets.QStackedWidget):
         elif button.text() == "Close":
             self.close()
 
-if __name__ == "__main__":
-    # import qdarkstyle
 
+if __name__ == "__main__":
+    import qdarkstyle
     app = QtWidgets.QApplication([])
     w = User_Analyser()
     w.show()
-    # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     app.exec_()
